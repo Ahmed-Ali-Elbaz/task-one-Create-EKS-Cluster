dd513f686baebe9f6f51c15418d30680df4533b3
Ahmed-Ali-Elbaz <ahmed.ali.elbaz.moahmed@gmail.com>
2023-03-28 14:23:44 +0200

    add some comments to vpc Module

--- c3-02-vpc-module.tf
+++ c3-02-vpc-module.tf
@@ -18,12 +18,16 @@ module "vpc" {
   private_subnets       = var.vpc_private_subnets
   
   # NAT Gateway - Outbound traffic
-  enable_nat_gateway    = var.vpc_enable_nat_gateway
+  enable_nat_gateway    = var.vpc_enable_nat_gateway 
   single_nat_gateway    = var.vpc_single_nat_gateway
 
   # VPC DNS Parameters
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
+  enable_dns_hostnames  = true # argument enables or disables DNS hostnames for the VPC. When set to "true" instances launched in the VPC are assigned a DNS hostname, which is resolved using Amazon-provided DNS servers. When set to "false" instances are not assigned a DNS hostname.
+  enable_dns_support    = true # argument enables or disables DNS resolution for the VPC. When set to "true" instances launched in the VPC can resolve DNS hostnames. When set to "false" instances cannot resolve DNS hostnames.
+  /*
+    Both allow the instance to resolve DNS hostnames and be assigned a DNS hostname within the VPC.
+    This can be useful for internal communication between instances within the VPC, as well as for connecting to external services using DNS hostnames.
+  */
 
   # tags = local.common_tags
   vpc_tags = local.common_tags
