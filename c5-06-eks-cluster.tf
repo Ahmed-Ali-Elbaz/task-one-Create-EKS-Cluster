3821850d7f9ad617d52b46f607723ee4d683719a
Ahmed-Ali-Elbaz <ahmed.ali.elbaz.moahmed@gmail.com>
2023-03-29 00:18:27 +0200

    fix some errors and make sure everything is working

--- c5-06-eks-cluster.tf
+++ c5-06-eks-cluster.tf
@@ -2,12 +2,12 @@
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "${local.name}-${var.cluster_name}"
-  role_arn = aws_iam_role.eks_master_role
+  role_arn = aws_iam_role.eks_master_role.arn
   version = var.cluster_version
 
 
   vpc_config {
-    subnet_ids              = flatten([module.vpc.public_subnets[*].id, module.vpc.private_subnets[*].id])
+    subnet_ids              = flatten([module.vpc.public_subnets, module.vpc.private_subnets])
     endpoint_private_access = var.cluster_endpoint_private_access
     endpoint_public_access  = var.cluster_endpoint_public_access
     public_access_cidrs     = var.cluster_endpoint_public_access_cidrs
